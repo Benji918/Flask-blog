27c5cf015550fa266ad6622bb2a80d8176dfe2ee
Benjamin <kodiugos@gmail.com>
2022-08-25 18:21:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ csrf = CSRFProtect(app)
 csrf.init_app(app)
 
 # CONNECT TO DB
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1, "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
